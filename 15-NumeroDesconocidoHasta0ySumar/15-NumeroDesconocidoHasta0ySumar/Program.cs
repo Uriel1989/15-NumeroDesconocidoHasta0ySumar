c8f04fcc8da2446b739516cbbb306f8cf6d086b9
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 20:30:26 -0300

    Add files via upload

---
+++ 15-NumeroDesconocidoHasta0ySumar/15-NumeroDesconocidoHasta0ySumar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _15_NumeroDesconocidoHasta0ySumar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n, b;
+
+            b = 0;
+
+            Console.WriteLine("Ingrese un numero entero: ");
+
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 0; i < n; i++)
+            {
+
+
+                b = b + i;
+
+
+            }
+            Console.WriteLine("La suma de los numeros anteriores al numero ingresado es: " + b);
+            Console.ReadKey();
+        }
+    }
+}
